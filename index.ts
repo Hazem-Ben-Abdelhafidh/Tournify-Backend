737669db027b6e4acb38d4b7eb3e13cca8e668e5
Hazem-Ben-Abdelhafidh <morbidvisions@yahoo.com>
2022-08-04 02:11:07 +0100

    configuring index.Ts

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import router from "./routes/userRoutes";
+import tournamentRouter from "./routes/tournamentsRoutes";
+import AppError from "./utils/appError";
+import helmet from "helmet";
+process.on("uncaughtException", (err) => {
+  console.log(err.name);
+  console.log(err.message);
+  console.log("shutting down...");
+  process.exit(1);
+});
 
 const app = express();
 const port = 5000;
@@ -21,8 +32,21 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
+app.use(helmet());
 app.listen(port, () => {
   console.log("Application listening on port: ", port);
 });
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+});
+
 app.use("/users", router);
+app.use("/tournaments", tournamentRouter);
+process.on("unhandledRejection", (err: any) => {
+  console.log(err.name);
+  console.log(err.message);
+  console.log("shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
