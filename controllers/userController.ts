728b1d702272b2f3ccc4d07adc8d6cb9408afc1b
Hazem-Ben-Abdelhafidh <morbidvisions@yahoo.com>
2022-08-17 21:58:03 +0100

    get User endpoint

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,2 +1,21 @@
 import express, { Request, Response, NextFunction } from "express";
-import { PrismaClient } from "@prisma/client";
+import catchAsync from "../utils/catchAsync";
+import HttpStatusCode from "../utils/constants/httpStatusCodes";
+import prisma from "../utils/prisma";
+
+export const getUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.params.id,
+      },
+      select: {
+        name: true,
+        id: true,
+      },
+    });
+    res.status(HttpStatusCode.ACCEPTED).json({
+      user,
+    });
+  }
+);
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,6 +1,8 @@
 import express from "express";
-import { signup, login } from "../controllers/authController";
+import { signup, login, protect } from "../controllers/authController";
+import { getUser } from "../controllers/userController";
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/getUser/:id", protect, getUser);
 export default router;
